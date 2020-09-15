fa3c97c8bba61d6c3b62352740b7ee009880d803
Mobin Kakar <mobin.kakar@wsabronx.org>
2020-09-15 11:03:32 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
 def say_hello(name ="Ruby Programmer")
   puts "Hello #{name}!"
+=======
+def say_hello(name! = "Ruby programmer")
+  puts "Hello, #{name!}"
+>>>>>>> 7646a12ef9fb6ffba9c9be5b0c16611db702b786
 end
